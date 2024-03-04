862b2b565cfb4d5edc7fd23004c8c0da5decfec5
RPravin86 <62685733+RPravin86@users.noreply.github.com>
2024-03-04 15:42:47 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,1 +1,19 @@
-Test File
+import java.io.*;
+import java.util.*;
+import java.lang.StringBuilder;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        String B = new StringBuilder(A).reverse().toString();
+        if(A.equalsIgnoreCase(B)){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }        
+    }
+}
